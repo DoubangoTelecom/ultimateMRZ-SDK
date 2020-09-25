1ef4b1782414d246153a5a718a10579a65bf1cef
Doubango Telecom <github@doubango.org>
2020-09-25 04:29:35 +0200

    - Fix typos in the documentation - Use bot 'M' and 'MN' to generate the runtime key on Linux x86-64 (not backward compatible)

--- samples/c++/runtimeKey/main.cxx
+++ samples/c++/runtimeKey/main.cxx
@@ -10,6 +10,7 @@
 		runtimeKey \
 			[--json <json-output:bool>] \
 			[--assets <path-to-assets-folder>]
+			[--type <host-type>]
 
 	Example:
 		runtimeKey \
@@ -40,13 +41,17 @@ int main(int argc, char *argv[])
 	if (args.find("--json") != args.end()) {
 		rawInsteadOfJSON = (args["--json"].compare("true") != 0);
 	}
+	
+	// Build JSON string
 	std::string jsonConfig;
 	if (args.find("--assets") != args.end()) {
-		jsonConfig = std::string("{ \"assets_folder\": \"") + args["--assets"] + std::string("\" }"); 
+		jsonConfig += std::string("\"assets_folder\": \"") + args["--assets"] + std::string("\""); 
 	}
-	else {
-		jsonConfig = "{}";
+	if (args.find("--type") != args.end()) {
+		jsonConfig += (jsonConfig.empty() ? "" : ",") 
+			+ std::string("\"host_type\": \"") + args["--type"] + std::string("\"");
 	}
+	jsonConfig = "{" + jsonConfig + "}";
 
 	// Initialize the engine
 	ULTMRZ_SDK_ASSERT(UltMrzSdkEngine::init(jsonConfig.c_str()).isOK());
@@ -88,6 +93,7 @@ static void printUsage(const std::string& message /*= ""*/)
 		"\n"
 		"--json: Whether to output the runtime license key as JSON string intead of raw string. Default: true.\n"
 		"--assets: Path to the assets folder containing the configuration files and models. Default value is the current folder.\n"
+		"--type: Defines how the license is attached to the machine/host. Possible values are 'aws-instance' or 'aws-byol'. Default: null.\n"
 		"********************************************************************************\n"
 	);
 }
